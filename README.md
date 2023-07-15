dc215e88dad44bbd39f965f4ae143382c8017059
Aurel Medvegy <medvegy@turnonline.biz>
2023-07-15 12:31:07 +0200

    Initial App Engine template based on the Micronaut framework.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# app-engine-java-micronaut
+# ConnecSys Micronaut App Template
+
 ConnecSys app template based on the Micronaut framework. Configured for Google App Engine Java 17 standard environment.
+
+## Micronaut 3.9.4 Documentation
+
+- [User Guide](https://docs.micronaut.io/3.9.4/guide/index.html)
+- [API Reference](https://docs.micronaut.io/3.9.4/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/3.9.4/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/latest/index.html)
+- [Using the App Engine Maven Plugin](https://cloud.google.com/appengine/docs/standard/java-gen2/using-maven)
+
+---
+
+## App Template prerequisites
+
+* Java 17 (temurin-17)
+* Maven installed, check with `mvn -v`
+* gcloud CLI app-engine-java component: `gcloud components install app-engine-java`
+* Check the target GCP project: `gcloud config list`
+    * Optionally set the target GCP project: `gcloud config set project [PROJECT_ID]`
+
+## Building and Deploying locally by Maven plugin
+
+```
+mvn package appengine:deploy
+```
---
+++ src/test/java/biz/turnonline/ecosystem/DefaultControllerTest.java
@@ -0,0 +1,40 @@
+package biz.turnonline.ecosystem;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+public class DefaultControllerTest
+{
+    @Inject
+    @Client( "/" )
+    HttpClient client;
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks()
+    {
+        Assertions.assertTrue( application.isRunning() );
+    }
+
+    @Test
+    public void testGet()
+    {
+        HttpRequest<String> request = HttpRequest.GET( "/" );
+        String body = client.toBlocking().retrieve( request );
+
+        assertNotNull( body );
+        assertEquals( "Hello World!", body );
+    }
+}
